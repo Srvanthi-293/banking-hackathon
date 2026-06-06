dd2fa6d73d617a7f55deb4ec687317bbc31048b3
SAI NIKITH <snikith6@gmail.com>
2026-06-05 23:34:23 -0700

    Transaction controller added

--- FraudRuleChecker/src/main/java/com/virtusa/FraudRuleChecker/controller/TransactionController.java
+++ FraudRuleChecker/src/main/java/com/virtusa/FraudRuleChecker/controller/TransactionController.java
@@ -1,4 +1,66 @@
 package com.virtusa.FraudRuleChecker.controller;
 
+import com.virtusa.FraudRuleChecker.entity.Transaction;
+import com.virtusa.FraudRuleChecker.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> addTransaction(
+            @RequestBody Transaction transaction) {
+
+        return ResponseEntity.ok(
+                transactionService.saveTransaction(transaction));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+
+        return ResponseEntity.ok(
+                transactionService.getAllTransactions());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                transactionService.getTransactionById(id));
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadCsv(
+            @RequestParam("file") MultipartFile file) {
+
+        transactionService.uploadCsv(file);
+
+        return ResponseEntity.ok(
+                "CSV uploaded successfully");
+    }
+
+    @PostMapping("/analyze/{id}")
+    public ResponseEntity<?> analyzeTransaction(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                transactionService.analyzeTransaction(id));
+    }
+
+    @PostMapping("/analyze-all")
+    public ResponseEntity<?> analyzeAllTransactions() {
+
+        return ResponseEntity.ok(
+                transactionService.analyzeAllTransactions());
+    }
 }
