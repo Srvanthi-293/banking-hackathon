f8f1cf9685deb40e2edffed5b25d36dfe0ede1fe
manikanta5678 <147979623+manikanta5678@users.noreply.github.com>
2026-06-06 21:23:48 +0530

    complete project structure

--- FraudRuleChecker/src/main/java/com/virtusa/FraudRuleChecker/service/ExcelReaderService.java
+++ FraudRuleChecker/src/main/java/com/virtusa/FraudRuleChecker/service/ExcelReaderService.java
@@ -1,9 +1,17 @@
 package com.virtusa.FraudRuleChecker.service;
 
+import com.virtusa.FraudRuleChecker.dto.ExcelTransactionDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ExcelReaderService {
