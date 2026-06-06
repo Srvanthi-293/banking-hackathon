3aaaec55e40624c0ca0c014c941189e2c7be6b40
manikanta5678 <147979623+manikanta5678@users.noreply.github.com>
2026-06-06 18:52:09 +0530

    complete project structure

--- FraudRuleChecker/src/main/java/com/virtusa/FraudRuleChecker/entity/Transaction.java
+++ FraudRuleChecker/src/main/java/com/virtusa/FraudRuleChecker/entity/Transaction.java
@@ -1,34 +1,32 @@
 package com.virtusa.FraudRuleChecker.entity;
 
-public class Transaction {
-    import jakarta.persistence.*;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
-    @Entity
-    @Table(name = "transactions")
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public class Transaction {
+@Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Transaction {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String customerId;
+    private String customerId;
 
-        private Double amount;
+    private Double amount;
 
-        private String payee;
+    private String payee;
 
-        private LocalDateTime transactionTime;
+    private LocalDateTime transactionTime;
 
-        private String homeCity;
+    private String homeCity;
 
-        private String transactionCity;
-    }
+    private String transactionCity;
 }
