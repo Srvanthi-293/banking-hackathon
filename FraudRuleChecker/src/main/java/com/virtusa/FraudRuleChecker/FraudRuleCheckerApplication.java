75b14894b5ab102833a47f46e32131b780579650
Boobathy R <hungrylearner2002@gmail.com>
2026-06-06 18:35:02 +0530

    Initial banking hackathon project

---
+++ FraudRuleChecker/src/main/java/com/virtusa/FraudRuleChecker/FraudRuleCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.FraudRuleChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudRuleCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudRuleCheckerApplication.class, args);
+	}
+
+}
---
+++ FraudRuleChecker/src/test/java/com/virtusa/FraudRuleChecker/FraudRuleCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.FraudRuleChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudRuleCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
